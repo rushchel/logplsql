bd7ebb3cab9cfab87b4f00e3d02962d156288662
Rustam Shakirov <rushchel@gmail.com>
2018-01-13 15:47:52 +0600

    added public synonym

--- log4plsql/sql/install_sys/grant_after_installation.sql
+++ log4plsql/sql/install_sys/grant_after_installation.sql
@@ -3,9 +3,14 @@ set verify off
 ACCEPT V_USER CHAR PROMPT 'Enter the user name:'
 
 GRANT EXECUTE ON &V_USER..PLOG_OUT_AQ TO PUBLIC;
-GRANT EXECUTE ON &V_USER..PLOG_PARAM TO PUBLIC;
+GRANT EXECUTE ON &V_USER..PLOGPARAM TO PUBLIC;
 GRANT EXECUTE ON &V_USER..PLOG TO PUBLIC;
 
+create public synonym plog_out_aq for &V_USER..plog_out_aq;
+create public synonym plogparam for &V_USER..plogparam;
+create public synonym plog for &V_USER..plog;
+create public synonym tlog for &V_USER..tlog;
+create public synonym vlog for &V_USER..vlog;
 
 set verify on
 
